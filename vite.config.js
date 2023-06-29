9b9e0ef59d09ac341775b18ec0492614dc134680
SterlingBoom <bummiecarez@gmail.com>
2023-06-29 21:17:53 +0100

    saved

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,15 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString()
+          }
+        },
       },
     },
   },
