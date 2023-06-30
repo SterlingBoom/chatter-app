0ef3efc46738598ab267b855dfec6d2899645790
SterlingBoom <bummiecarez@gmail.com>
2023-06-30 07:03:12 +0100

    saved changes

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cssInjectedByJsPlugin()],
-  css: {
-    postcss: {
-      plugins: [autoprefixer],
-    },
-  },
+  plugins: [react()],
   build: {
     target: 'es2021',
     rollupOptions: {
